6180fd069b23dcb6d007b31cda84ab885820b2ae
Karl <kturrell@hotmail.com>
2015-11-11 21:01:45 +0000

    Add remaining Person fields with a zipcode regex validator

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
+import re
 from flask import Flask
 from flask_restful import reqparse, Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+# Validators
+def valid_zipcode(value):
+   if re.match(r"^[0-9]{5}(?:-[0-9]{4})?$", value) is None:
+      raise ValueError("Invalid zipcode format")
+   return value
+
+# Parsers and internal data structures
 people = {}
 pid = 1
 parser = reqparse.RequestParser()
-parser.add_argument("name")
+parser.add_argument("firstname", required=True)
+parser.add_argument("lastname", required=True)
+parser.add_argument("dateofbirth", required=True)
+parser.add_argument("zipcode", type=valid_zipcode, required=True)
 
+# REST API resources
 class Person(Resource):
    def get(self, person_id):
       return {person_id: people[person_id]}
@@ -27,12 +39,18 @@ class PersonList(Resource):
       args = parser.parse_args()
       person_id = pid
       pid += 1
-      people[person_id] = {"name": args["name"]}
+      people[person_id] = {
+         "firstname": args["firstname"],
+         "lastname": args["lastname"],
+         "dateofbirth": args["dateofbirth"],
+         "zipcode": args["zipcode"]
+      }
       return people[person_id], 201
 
 api.add_resource(PersonList, "/persons")
 api.add_resource(Person, "/person/<int:person_id>")
 
+# Static content and application launcher
 @app.route("/")
 def home():
    return "Homepage"
